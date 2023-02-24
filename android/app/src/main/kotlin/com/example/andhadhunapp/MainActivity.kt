a61f43fd659404a0c4273f17bf0bca7366892c84
tusharb <tusharbansal@iitbhilai.ac.in>
2023-02-24 19:14:19 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/andhadhunapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.andhadhunapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/questions.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Questions extends StatefulWidget {
+  const Questions({Key? key}) : super(key: key);
+
+  @override
+  State<Questions> createState() => _QuestionsState();
+}
+
+class _QuestionsState extends State<Questions> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('questions'),
+    );
+  }
+}
