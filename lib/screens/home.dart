c078ec9d76c75764f5d5b832a563aa6b3e973718
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2023-02-27 10:26:42 +0530

    location improved

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,5 @@
+import 'package:andhadhunapp/services/location.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_tts/flutter_tts.dart';
-import 'package:geolocator/geolocator.dart';
-
-import 'LocAuth.dart';
-
-
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -14,18 +9,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  @override
-  FlutterTts flutterTts = FlutterTts();
-  Future<Position> _getCurrentPosition() async{
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    return position;
+  void initState() async {
+    var userLocation = await UserLocation().determinePosition();
+    print(userLocation);
   }
 
+  @override
+  // FlutterTts flutterTts = FlutterTts();
+  // Future<Position> _getCurrentPosition() async{
+  //   Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+  //   return position;
+  // }
+
   Widget build(BuildContext context) {
-    return (_getCurrentPosition() == _getCurrentPosition())?Scaffold(
-      body: LocAuth(),
-    ):Scaffold(
-      body: Text('Location not found'),
+    // return (_getCurrentPosition() == _getCurrentPosition())
+    //     ? Scaffold(
+    //         body: LocAuth(),
+    //       )
+    //     : Scaffold(
+    //         body: Text('Location not found'),
+    //       );
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Andadhun'),
+      ),
+      body: Container(
+        child: Text('This is the body'),
+      ),
     );
   }
 }
