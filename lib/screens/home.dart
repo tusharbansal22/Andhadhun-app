888d93ab312b867ad1ea690705d02e607fb32900
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2023-03-25 12:24:24 +0530

    some cleaning

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,86 +12,21 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  // String mobileNumber = '123';
-  // List<SimCard> _simCard = <SimCard>[];
-
   void getCurrentPosition() async {
     var userLocation = await UserLocation().determinePosition();
     print(userLocation);
   }
 
   FlutterTts flutterTts = FlutterTts();
-  // Future<Position> _getCurrentPosition() async {
-  //   Position position = await Geolocator.getCurrentPosition(
-  //       desiredAccuracy: LocationAccuracy.high);
-  //   var lat = position.latitude;
-  //   var long = position.longitude;
-  //   print(lat);
-  //   // TODO: Use http package to send api call to server
-  //   return position;
-  // }
 
   @override
   void initState() {
     super.initState();
     getCurrentPosition();
-    // MobileNumber.listenPhonePermission((isPermissionGranted) {
-    //   // if (isPermissionGranted) {
-    //   initMobileNumberState();
-    //   // } else {
-    //   //   print("No permission.");
-    //   // }
-    // });
   }
-
-  // Future<void> initMobileNumberState() async {
-  //   // if (!await MobileNumber.hasPhonePermission) {
-  //   await MobileNumber.requestPhonePermission;
-  //   // return;
-  //   // }
-  //   // Platform messages may fail, so we use a try/catch PlatformException.
-  //   try {
-  //     mobileNumber = (await MobileNumber.mobileNumber)!;
-  //     _simCard = (await MobileNumber.getSimCards)!;
-  //     print(mobileNumber);
-  //   } on PlatformException catch (e) {
-  //     debugPrint("Failed to get mobile number because of '${e.message}'");
-  //   }
-  //
-  //   // If the widget was removed from the tree while the asynchronous platform
-  //   // message was in flight, we want to discard the reply rather than calling
-  //   // setState to update our non-existent appearance.
-  //   if (!mounted) return;
-  //
-  //   setState(() {});
-  // }
 
   @override
   Widget build(BuildContext context) {
-    // return (_getCurrentPosition() == _getCurrentPosition())
-    //     ? Scaffold(
-    //         body: LocAuth(),
-    //       )
-    //     : Scaffold(
-    //         body: Text('Location not found'),
-    //       );
     return LocAuth();
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     title: Text('Andhadhun'),
-    //     centerTitle: true,
-    //   ),
-    //   body: ElevatedButton(
-    //       onPressed: () {
-    //         Navigator.pushNamed(context, "main_screen");
-    //       },
-    //       child: Text('hello')),
-    // );
   }
 }
-
-// FlutterTts flutterTts = FlutterTts();
-// Future<Position> _getCurrentPosition() async{
-//   Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-//   return position;
-// }
